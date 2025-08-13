732d32da2e7931d1c536a262570d4bc738efa10d
eritger1110 <eric.ritger@gmail.com>
2025-08-13 11:22:00 -0500

    Update eleventy.aih.js

--- eleventy.aih.js
+++ eleventy.aih.js
@@ -1,12 +1,17 @@
-module.exports = function(eleventyConfig) {
+// eleventy.aih.js
+module.exports = function (eleventyConfig) {
   eleventyConfig.addNunjucksFilter("date", (value, format = "yyyy") => {
     const d = value ? new Date(value) : new Date();
     if (format === "yyyy") return String(d.getFullYear());
     return d.toISOString();
   });
 
-  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
-  eleventyConfig.addPassthroughCopy({ "src/aih/_redirects": "_redirects" });
+  eleventyConfig.addPassthroughCopy({
+    "src/assets": "assets",
+    "styles.css": "styles.css",
+    "script.js": "script.js",
+    "src/aih/_redirects": "_redirects"
+  });
 
   return {
     dir: {
