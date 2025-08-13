1954fa999c4002b1c4401b31953b49ca34394296
eritger1110 <eric.ritger@gmail.com>
2025-08-13 11:11:53 -0500

    Update eleventy.cc.js

--- eleventy.cc.js
+++ eleventy.cc.js
@@ -1,23 +1,18 @@
-module.exports = function (eleventyConfig) {
+module.exports = function(eleventyConfig) {
   eleventyConfig.addNunjucksFilter("date", (value, format = "yyyy") => {
     const d = value ? new Date(value) : new Date();
     if (format === "yyyy") return String(d.getFullYear());
     return d.toISOString();
   });
 
-  eleventyConfig.addPassthroughCopy({
-    "src/assets": "assets",
-    "styles.css": "styles.css",
-    "script.js": "script.js",
-    "src/cc/_redirects": "_redirects"
-    // "src/cc/robots.txt": "robots.txt",
-  });
+  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+  eleventyConfig.addPassthroughCopy({ "src/cc/_redirects": "_redirects" });
 
   return {
     dir: {
       input: "src/cc",
       includes: "../shared",
-      output: "dist/cc",
-    },
+      output: "dist/cc"
+    }
   };
 };
