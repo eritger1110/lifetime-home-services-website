4c41db027df397bafad9d54e69cd746651d46154
eritger1110 <eric.ritger@gmail.com>
2025-08-13 15:35:42 -0500

    Update eleventy.cc.js

--- eleventy.cc.js
+++ eleventy.cc.js
@@ -16,7 +16,7 @@ module.exports = function (eleventyConfig) {
   return {
     dir: {
       input: "src/cc",
-      includes: "src/shared",
+      includes: "src/Lifetime/_includes/shared",
       output: "dist/cc"
     }
   };
