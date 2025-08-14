b32f4ad167b2d36d9ba582dffa6f65d01ea4faad
eritger1110 <eric.ritger@gmail.com>
2025-08-14 13:20:53 -0500

    Update eleventy.lifetime.js

--- eleventy.lifetime.js
+++ eleventy.lifetime.js
@@ -17,7 +17,7 @@ module.exports = function (eleventyConfig) {
   return {
     dir: {
       input: "src/lifetime",
-      includes: "../_includes",
+      includes: "../",
       output: "dist/lifetime",
     },
   };
