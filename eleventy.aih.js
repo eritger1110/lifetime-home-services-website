7e2d613d24102af1b470b389b552b1290077c36d
eritger1110 <eric.ritger@gmail.com>
2025-08-14 13:21:23 -0500

    Update eleventy.aih.js

--- eleventy.aih.js
+++ eleventy.aih.js
@@ -17,7 +17,7 @@ module.exports = function (eleventyConfig) {
   return {
     dir: {
       input: "src/aih",
-      includes: "../_includes",
+      includes: "../",
       output: "dist/aih",
     },
   };
