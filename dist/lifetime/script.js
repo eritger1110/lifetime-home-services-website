cf5c6af01a159944b9a1aa37b02663ad691db158
eritger1110 <eric.ritger@gmail.com>
2025-08-14 15:43:10 -0500

    update 8.14

---
+++ dist/lifetime/script.js
@@ -0,0 +1,5 @@
+// Simple "show phone hours" modal (optional: you can expand later)
+(function () {
+  // If you later add a modal, wire it here. Leaving as placeholder.
+  console.log("script.js loaded");
+})();
